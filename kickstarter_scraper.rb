bde6bad88fb8c1c214184f8e598925492eff235b
Alehandro Persaud <andrew041994@gmail.com>
2020-02-12 00:29:05 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,7 +5,7 @@ require 'nokogiri'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
 kickstarter = Nokogiri::HTML(html)
-# binding.pry
+binding.pry
 
 end
 create_project_hash
