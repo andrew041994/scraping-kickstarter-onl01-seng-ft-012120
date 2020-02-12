3de04542249a071f420ca5acc3096cbc47907c35
Alehandro Persaud <andrew041994@gmail.com>
2020-02-12 01:26:03 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,9 +8,10 @@ def create_project_hash
 
   projects = {}
 
-  # Iterate through the projects
   kickstarter.css("li.project.grid_4").each do |project|
-    projects[project] = {}
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
+  end
   end
 
   # return the projects hash
